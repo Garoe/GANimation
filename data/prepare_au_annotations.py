f5d0a67d27d10772014c862bc6d4c050c2aa13b1
Garoe <garoedp@gmail.com>
2018-10-24 15:42:35 +0100

    Fixed bug with prepare AU files

--- data/prepare_au_annotations.py
+++ data/prepare_au_annotations.py
@@ -12,10 +12,20 @@ parser.add_argument('-op', '--output_path', type=str, help='Output path')
 args = parser.parse_args()
 
 def get_data(filepaths):
+    # The first line contains the headers, the second the data
+    # In the second line the last au_c_size elements are the indicators of present of AU, and before them there are
+    # au_r_size indicators of intensity of the AU
+    line_elem = 711
+    au_c_size = 18
+    au_r_size = 17
+    start_idx = line_elem - (au_r_size + au_c_size)
+    end_idx = line_elem - au_c_size
     data = dict()
     for filepath in tqdm(filepaths):
         content = np.loadtxt(filepath, delimiter=', ', skiprows=1)
-        data[os.path.basename(filepath[:-4])] = content[2:19]
+        if content.ndim > 1:  # Found more than one face take only the first one
+            content = content[0]
+        data[os.path.basename(filepath[:-4])] = content[start_idx:end_idx]
 
     return data
 
